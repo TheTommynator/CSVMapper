5cee053487cf5846a6b6aaa80f8eb60f818235ef
Maximilian Enders <max.enders@gmail.com>
2025-02-17 13:54:21 +0100

    Initial Commit

---
+++ CSVMapper/CSVMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSVMapperApp.swift
+//  CSVMapper
+//
+//  Created by Maximilian Enders on 17.02.25.
+//
+
+import SwiftUI
+
+@main
+struct CSVMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
