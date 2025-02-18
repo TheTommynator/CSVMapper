2af3423e76e3b3287838c4963896611b699ebb06
Maximilian Enders <max.enders@gmail.com>
2025-02-18 10:02:36 +0100

    Initial Commit - 0.1

---
+++ CSVMapper/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  CSVMapper
+//
+//  Created by Maximilian Enders on 17.02.25.
+//
+
+import Foundation
---
+++ CSVMapper/PersonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PersonViewModel.swift
+//  CSVMapper
+//
+//  Created by Maximilian Enders on 17.02.25.
+//
+
+import Foundation
