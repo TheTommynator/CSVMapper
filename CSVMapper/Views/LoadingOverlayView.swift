6950f3f50485275bf05cddb1123777fd2b53eec0
Maximilian Enders <max.enders@gmail.com>
2025-02-20 12:48:26 +0100

    Version 0.3 Bessere Loading-Animation hinzugefügt

---
+++ CSVMapper/Views/LoadingOverlayView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingOverlayView.swift
+//  CSVMapper
+//
+//  Created by Maximilian Enders on 20.02.25.
+//
+
+import SwiftUI
+
+struct LoadingOverlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadingOverlayView()
+}
