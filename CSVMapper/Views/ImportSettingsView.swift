d470f8ebb9aa808e725ce6d6d28af86a3bc7c1d3
Maximilian Enders <max.enders@gmail.com>
2025-02-21 11:47:09 +0100

    Version 0.3.5 Settings-Screen eingefügt isPerson macht aktuell noch nichts

---
+++ CSVMapper/Views/ImportSettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  ImportSettingsVIew.swift
+//  CSVMapper
+//
+//  Created by Maximilian Enders on 21.02.25.
+//
+
+import SwiftUI
+
+struct ImportSettingsVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImportSettingsVIew()
+}
---
+++ CSVMapper/Views/TextInputFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  TextInputFieldVIew.swift
+//  CSVMapper
+//
+//  Created by Maximilian Enders on 21.02.25.
+//
+
+import SwiftUI
+
+struct TextInputFieldVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextInputFieldVIew()
+}
